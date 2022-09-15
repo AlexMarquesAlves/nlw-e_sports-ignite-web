f8a4515ebd1c8d95f41db792c75f15c868fd8495
¡Duque <atanaelleonardo@gmail.com>
2022-09-15 02:04:18 -0300

    🆕 LogoImg added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
 import logoImg from "./assets/Logo.png";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
     </div>
   );
