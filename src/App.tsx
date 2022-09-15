699530d642924480632e2057ed7213fdbba702bb
¡Duque <atanaelleonardo@gmail.com>
2022-09-15 02:08:17 -0300

    🆕 Button icon added

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,7 @@ function App() {
           </div>
 
           <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
             Publicar anúncio
           </button>
         </div>
